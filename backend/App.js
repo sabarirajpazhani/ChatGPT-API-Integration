e73e9da112335ec1faad38b10321c9a3d230630f
sabarirajpazhani <sabarirajpazhani@gmail.com>
2024-09-07 19:01:33 +0530

    Testing the API route

--- backend/App.js
+++ backend/App.js
@@ -4,6 +4,12 @@ const dotenv = require("dotenv");
 const path = require("path");
 dotenv.config({path:path.join(__dirname,'config','config.env')});
 
+const chat = require('./routes/chatRoute');
+
+app.use(express.json());
+
+app.use('/api/v1/',chat);
+
 
 app.listen(process.env.PORT, ()=>{
     console.log(`Server is Listening to Port ${process.env.PORT} in ${process.env.NODE_ENV}`);
